719391d8883e418e31cdca475a9895f692fd4aa1
Roman Bulhakau <bulgakovra.w@gmail.com>
2022-07-28 18:48:25 +0300

    Add body parser

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    }),
+);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Success',
+    });
+});
 
 app.listen(3001, () => {
     console.log('Server starts on port 3000!');
